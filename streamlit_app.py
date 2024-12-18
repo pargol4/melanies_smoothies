e13c8876ed031d0182739d298f6e380381e182b6
pargol4 <72155870+pargol4@users.noreply.github.com>
2024-12-18 08:55:07 -0600

    Update streamlit_app.py
    Added Nutrition Information subheader and updated API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-#    st.write(ingredients_list)
-#    st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -58,9 +58,6 @@ if ingredients_list:
 #        session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
